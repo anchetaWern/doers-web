3b0952e6c3d78fd420464a3fc42f762df905533d
Wern Ancheta <ancheta.wern@gmail.com>
2025-08-16 15:56:39 +0800

    [fix]

--- plugins/google-maps.client.js
+++ plugins/google-maps.client.js
@@ -1,10 +1,12 @@
 import { defineNuxtPlugin } from '#app'
 import VueGoogleMaps from '@fawmi/vue-google-maps'
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGoogleMaps, {
     load: {
-      key: 'AIzaSyAtcOhdTKP5ta6ZYVX5Vc1uhdc7jreHHgY',
+      key: apiKey,
     },
   })
 })
