e41f649e51903bfac71751db022a2016f95e3fc6
Wern Ancheta <ancheta.wern@gmail.com>
2025-08-21 10:48:22 +0800

    [fix] add vcalendar

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,10 +4,13 @@ import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
+import { VCalendar } from 'vuetify/labs/VCalendar'
+
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     components: {
       ...components,
+      VCalendar,
     },
     directives,
   })
