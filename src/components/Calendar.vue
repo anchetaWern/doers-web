0fc1bfdc6ffc7f504842c548d33ed66db923504c
Wern Ancheta <ancheta.wern@gmail.com>
2025-08-21 11:11:22 +0800

    [improvement] show where and when

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -10,7 +10,7 @@
         @click:event="onEventClick"
       />
 
-<v-menu
+      <v-menu
         v-model="menu"
         :activator="menuActivator"
         location="top"
@@ -21,7 +21,11 @@
             {{ selectedTitle }}
           </v-card-title>
           <v-card-text>
-            TODO: add times, location, more details
+            <ul class="ml-5">
+              <li>Where: {{ selectedAddress }}</li>
+              <li>When: {{ selectedTime }}</li>
+            </ul>
+            
           </v-card-text>
         </v-card>
       </v-menu>
@@ -44,10 +48,15 @@ const adapter = useDate()
 //
 const menu = ref(false)
 const selectedTitle = ref('')
+const selectedTime = ref('')
+const selectedAddress = ref('');
 const menuActivator = ref(null)
 
 function onEventClick(e) {
-  selectedTitle.value = e.target.innerText
+  const split = e.target.innerText.split('|');
+  selectedTitle.value = split[0];
+  selectedAddress.value = split[1];
+  selectedTime.value = split[2];
   menuActivator.value = e.target // anchor menu to clicked element
   menu.value = true
 }
@@ -56,20 +65,27 @@ function onEventClick(e) {
 function getEvents() {
   events.value = [
     {
-      title: 'Trash hunt with Philippine Navy @ HQ to Lingsat SFC',
+      title: 'Trash hunt with Philippine Navy | HQ to Lingsat SFC | Aug 19, 5:30am',
       start: new Date(2025, 7, 19, 5, 30),
       end: new Date(2025, 7, 19, 6, 30),
       color: 'blue',
     },
     {
-      title: 'Coastal cleanup @ brgy Cabarsican, Bacnotan LU',
+      title: 'Coastal cleanup | Brgy Cabarsican, Bacnotan LU | Aug 19, 6:00am',
       start: new Date(2025, 7, 19, 6, 0),
       end: new Date(2025, 7, 19, 7, 0),
       color: 'red',
       allDay: true,
     },
     {
-      title: 'Blood letting @ Mess Hall, Wallace Air Station, Poro SFC',
+      title: 'Tree Planting | Luna LU | Aug 24, 6:30am',
+      start: new Date(2025, 7, 24, 6, 30),
+      end: new Date(2025, 7, 24, 8, 0),
+      color: 'red',
+      allDay: true,
+    },
+    {
+      title: 'Blood letting | Mess Hall, Wallace Air Station, Poro SFC | Aug 29, 8:00am to 12:00pm',
       start: new Date(2025, 7, 29, 8, 0),
       end: new Date(2025, 7, 29, 12, 0),
       color: 'green',
