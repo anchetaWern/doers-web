f9818b4c74154b02d4ca0d7771b199b993ba4dbe
Wern Ancheta <ancheta.wern@gmail.com>
2025-08-31 17:56:58 +0800

    [revision] update our impact

--- src/sections/OurImpact.vue
+++ src/sections/OurImpact.vue
@@ -5,33 +5,33 @@
             <v-row justify="center">
             <v-col cols="12" md="auto">
                 <h2 class="text-h3 font-weight-bold text-center">Our Impact</h2>
-                <h3 class="text-h5 text-center">Since 2020</h3>
+                <h3 class="text-h5 text-center">Since 2024</h3>
             </v-col>
             </v-row>
 
             <!-- First Stats Row -->
             <v-row class="stats-row mt-5">
             <v-col cols="12" md="auto" class="text-center mb-4 mb-md-0">
-                <h2 class="text-h3">500+</h2>
-                <h3 class="text-h5">Clean-up volunteers</h3>
+                <h2 class="text-h3">742</h2>
+                <h3 class="text-h5">Daily beach clean ups</h3>
             </v-col>
 
             <v-col cols="12" md="auto" class="text-center mb-4 mb-md-0">
-                <h2 class="text-h3">100+</h2>
-                <h3 class="text-h5">Blood bags donated</h3>
+                <h2 class="text-h3">26</h2>
+                <h3 class="text-h5">Blood letting activities</h3>
             </v-col>
 
             <v-col cols="12" md="auto" class="text-center">
-                <h2 class="text-h3">2,000+ kg</h2>
-                <h3 class="text-h5">Garbage collected</h3>
+                <h2 class="text-h3">124</h2>
+                <h3 class="text-h5">Turtle patrolling days</h3>
             </v-col>
             </v-row>
 
             <!-- Second Stats Row -->
             <v-row justify="center" class="mt-5">
             <v-col cols="12" md="auto" class="text-center">
-                <h2 class="text-h3">1,000+</h2>
-                <h3 class="text-h5">Mangroves Planted & Parented</h3>
+                <h2 class="text-h3">26</h2>
+                <h3 class="text-h5">Tree planting activities</h3>
             </v-col>
             </v-row>
         </v-container>
